5b3df567e8112cec0368a583d3f307abcb73f352
zigmint <rupeshr053@gmail.com>
2025-06-02 00:55:47 +0530

    CH5 L7

--- main.go
+++ main.go
@@ -1,33 +1,29 @@
 package main
 
-type employee interface {
-	getName() string
-	getSalary() int
+func (e email) cost() int {
+	if e.isSubscribed {
+		return 2 * len(e.body)
+	}
+	return 5 * len(e.body)
 }
 
-type contractor struct {
-	name         string
-	hourlyPay    int
-	hoursPerYear int
+func (e email) format() string {
+	str := "'" + e.body + "'" + " | "
+	if e.isSubscribed {
+		return str + "Subscribed"
+	}
+	return str + "Not Subscribed"
 }
 
-func (c contractor) getName() string {
-	return c.name
+type expense interface {
+	cost() int
 }
 
-func (c contractor) getSalary() int {
-	return c.hourlyPay * c.hoursPerYear
+type formatter interface {
+	format() string
 }
 
-type fullTime struct {
-	name   string
-	salary int
-}
-
-func (ft fullTime) getSalary() int {
-	return ft.salary
-}
-
-func (ft fullTime) getName() string {
-	return ft.name
+type email struct {
+	isSubscribed bool
+	body         string
 }
