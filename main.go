658a3762e1034ce06388b29db9e01d3b97a48141
zigmint <rupeshr053@gmail.com>
2025-06-02 01:18:01 +0530

    CH6 L10

--- main.go
+++ main.go
@@ -1,29 +1,51 @@
 package main
 
-func (e email) cost() int {
-	if e.isSubscribed {
-		return 2 * len(e.body)
+func getExpenseReport(e expense) (string, float64) {
+	email, ok := e.(email)
+	if ok {
+		return email.toAddress, email.cost()
 	}
-	return 5 * len(e.body)
+	sms, ok := e.(sms)
+	if ok {
+		return sms.toPhoneNumber, sms.cost()
+	}
+	return "", 0.0
 }
 
-func (e email) format() string {
-	str := "'" + e.body + "'" + " | "
-	if e.isSubscribed {
-		return str + "Subscribed"
-	}
-	return str + "Not Subscribed"
-}
+// don't touch below this line
 
 type expense interface {
-	cost() int
-}
-
-type formatter interface {
-	format() string
+	cost() float64
 }
 
 type email struct {
 	isSubscribed bool
 	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
 }
