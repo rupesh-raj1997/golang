41cb57bc8d4e3af85f0936c7564a0070e255eb76
zigmint <rupeshr053@gmail.com>
2025-06-01 23:51:11 +0530

    CH4 L10

--- main.go
+++ main.go
@@ -1,27 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
-
-type contact struct {
-	sendingLimit int32
-	age          int32
-	userID       string
+type Membership struct {
+	Type             string
+	MessageCharLimit int
 }
 
-type perms struct {
-	permissionLevel int
-	canSend         bool
-	canReceive      bool
-	canManage       bool
+type User struct {
+	Name string
+	Membership
 }
 
-func main() {
-
-	typ := reflect.TypeOf(contact{})
-	fmt.Printf("Struct is %d bytes\n", typ.Size())
-	typ = reflect.TypeOf(perms{})
-	fmt.Printf("Struct is %d bytes\n", typ.Size())
+func newUser(name string, membershipType string) User {
+	user := User{
+		Name: name,
+		Membership: Membership{
+			Type:             membershipType,
+			MessageCharLimit: 100,
+		},
+	}
+	if membershipType == "premium" {
+		user.Membership.MessageCharLimit = 1000
+	}
+	return user
 }
