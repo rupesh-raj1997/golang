b5e4d24ec3c41f30105c87390d08b419e78d0e99
zigmint <rupeshr053@gmail.com>
2025-06-01 23:36:42 +0530

    CH4 L6

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-type sender struct {
-	user
-	rateLimit int
+type authenticationInfo struct {
+	username string
+	password string
 }
 
-type user struct {
-	name   string
-	number int
+// create the method below
+func (a authenticationInfo) getBasicAuth() string {
+	return "Authorization: Basic " + a.username + ":" + a.password
 }
