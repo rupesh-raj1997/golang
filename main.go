adc4efa1246757601b64d058240ae394a6b2e29f
zigmint <rupeshr053@gmail.com>
2025-06-02 13:17:49 +0530

    CH6 L2

--- main.go
+++ main.go
@@ -4,27 +4,8 @@ import (
 	"fmt"
 )
 
-func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
-	custCost, err := sendSMS(msgToCustomer)
-	if err != nil {
-		return 0, err
-	}
+// SMS that costs $COST to be sent to 'RECIPIENT' can not be sent
 
-	custSpouseCost, err := sendSMS(msgToSpouse)
-	if err != nil {
-		return 0, err
-	}
-
-	return custCost + custSpouseCost, nil
-}
-
-// don't edit below this line
-
-func sendSMS(message string) (int, error) {
-	const maxTextLen = 25
-	const costPerChar = 2
-	if len(message) > maxTextLen {
-		return 0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
-	}
-	return costPerChar * len(message), nil
+func getSMSErrorString(cost float64, recipient string) string {
+	return fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent", cost, recipient)
 }
