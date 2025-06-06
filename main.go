e56aa4b713f66baad8b4b99df284d2f1bcc61e39
zigmint <rupeshr053@gmail.com>
2025-06-06 17:00:09 +0530

    CH8 l2

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
-func getMessageWithRetries(primary, secondary, tertiary string) ([3]string, [3]int) {
-	var costs [3]int
-	costs[0] = len(primary)
-	costs[1] = costs[0] + len(secondary)
-	costs[2] = costs[1] + len(tertiary)
-	messages := [3]string{primary, secondary, tertiary}
-	return messages, costs
+import (
+	"errors"
+)
+
+const (
+	planFree = "free"
+	planPro  = "pro"
+)
+
+var errUnsupportedPlan = errors.New("unsupported plan")
+
+func getMessageWithRetriesForPlan(plan string, messages [3]string) ([]string, error) {
+	// ?
+	switch plan {
+	case planFree:
+		return messages[:2], nil
+	case planPro:
+		return messages[:], nil
+	default:
+		return nil, errUnsupportedPlan
+	}
 }
