f4912f88df4fe00b25bcbd07168775fc6332a643
zigmint <rupeshr053@gmail.com>
2025-06-08 19:11:22 +0530

    CH9 L1

--- main.go
+++ main.go
@@ -1,31 +1,23 @@
 package main
 
-import "strings"
+import "errors"
 
-type sms struct {
-	id      string
-	content string
-	tags    []string
+func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
+	if len(names) != len(phoneNumbers) {
+		err := errors.New("invalid sizes")
+		return nil, err
+	}
+
+	userMap := make(map[string]user)
+
+	for i, name := range names {
+		userMap[name] = user{name: name, phoneNumber: phoneNumbers[i]}
+	}
+
+	return userMap, nil
 }
 
-func tagMessages(messages []sms, tagger func(sms) []string) []sms {
-	var data []sms
-	for _, msg := range messages {
-		msg.tags = tagger(msg)
-		data = append(data, msg)
-	}
-	return data
-}
-
-func tagger(msg sms) []string {
-	tags := []string{}
-
-	if strings.Contains(strings.ToLower(msg.content), "urgent") {
-		tags = append(tags, "Urgent")
-	}
-	if strings.Contains(strings.ToLower(msg.content), "sale") {
-		tags = append(tags, "Promo")
-	}
-
-	return tags
+type user struct {
+	name        string
+	phoneNumber int
 }
