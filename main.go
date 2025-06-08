78b52456fb184b7ae9a1539f9d226344f5ec0d06
zigmint <rupeshr053@gmail.com>
2025-06-08 14:31:40 +0530

    CH8 L13

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
-func indexOfFirstBadWord(msg []string, badWords []string) int {
-	for i, m := range msg {
-		for _, bword := range badWords {
-			if m == bword {
-				return i
-			}
+func createMatrix(rows, cols int) [][]int {
+	matrix := make([][]int, rows)
+
+	for i := 0; i < rows; i++ {
+		matrix[i] = make([]int, cols)
+		for j := 0; j < cols; j++ {
+			matrix[i][j] = i * j
 		}
 	}
-	return -1
+
+	return matrix
 }
