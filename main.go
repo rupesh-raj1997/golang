8fbffd08b913aebaa867377edf772a47448702ee
zigmint <rupeshr053@gmail.com>
2025-06-09 20:54:19 +0530

    CH10 L6

--- main.go
+++ main.go
@@ -1,27 +1,16 @@
 package main
 
-type Analytics struct {
-	MessagesTotal     int
-	MessagesFailed    int
-	MessagesSucceeded int
-}
+import (
+	"strings"
+)
 
-type Message struct {
-	Recipient string
-	Success   bool
-}
-
-// Write a getMessageText function.
-// It should accept a pointer to an Analytics struct and a Message struct (not a pointer).
-// It should look at the Success field of the Message struct and, based on that,
-// increment the MessagesTotal, MessagesSucceeded, or MessagesFailed fields of
-// the Analytics struct as appropriate.
-
-func getMessageText(anal *Analytics, msg Message) {
-	if msg.Success {
-		(*anal).MessagesSucceeded++
-	} else {
-		(*anal).MessagesFailed++
+func removeProfanity(message *string) {
+	if message == nil {
+		return
 	}
-	(*anal).MessagesTotal++
+	messageVal := *message
+	messageVal = strings.ReplaceAll(messageVal, "fubb", "****")
+	messageVal = strings.ReplaceAll(messageVal, "shiz", "****")
+	messageVal = strings.ReplaceAll(messageVal, "witch", "*****")
+	*message = messageVal
 }
