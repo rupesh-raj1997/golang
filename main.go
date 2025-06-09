d58e505cb27761b4f9b1a3fc735f91e18192c6fb
zigmint <rupeshr053@gmail.com>
2025-06-09 15:31:26 +0530

    CH9 L5

--- main.go
+++ main.go
@@ -1,23 +1,11 @@
 package main
 
-import "errors"
+func getCounts(messagedUsers []string, validUsers map[string]int) {
+	for _, user := range messagedUsers {
+		count, ok := validUsers[user]
 
-func deleteIfNecessary(users map[string]user, name string) (deleted bool, err error) {
-	elem, ok := users[name]
-	if !ok {
-		return false, errors.New("not found")
+		if ok {
+			validUsers[user] = count + 1
+		}
 	}
-
-	if !elem.scheduledForDeletion {
-		return false, nil
-	}
-
-	delete(users, name)
-	return true, nil
-}
-
-type user struct {
-	name                 string
-	number               int
-	scheduledForDeletion bool
 }
