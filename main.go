885cc3b9ad2e475312456bdf4ec3f00fed33a99d
zigmint <rupeshr053@gmail.com>
2025-06-10 09:37:47 +0530

    CH10 L11

--- main.go
+++ main.go
@@ -1,13 +1,44 @@
 package main
 
-func (e *email) setMessage(newMessage string) {
-	e.message = newMessage
+import "errors"
+
+type customer struct {
+	id      int
+	balance float64
 }
 
-// don't edit below this line
+type transactionType string
 
-type email struct {
-	message     string
-	fromAddress string
-	toAddress   string
+const (
+	transactionDeposit    transactionType = "deposit"
+	transactionWithdrawal transactionType = "withdrawal"
+)
+
+type transaction struct {
+	customerID      int
+	amount          float64
+	transactionType transactionType
 }
+
+func updateBalance(cust *customer, trans transaction) error {
+	switch trans.transactionType {
+	case transactionDeposit:
+		(*cust).balance += trans.amount
+	case transactionWithdrawal:
+		if trans.amount > (*cust).balance {
+			return errors.New("insufficient funds")
+		}
+		(*cust).balance -= trans.amount
+	default:
+		return errors.New("unknown transaction type")
+	}
+	return nil
+}
+
+// Don't touch above this line
+
+// ?
+// alice := customer{id: 1, balance: 100.0}
+// deposit := transaction{customerID: 1, amount: 50, transactionType: transactionDeposit}
+
+// updateBalance(&alice, deposit)
