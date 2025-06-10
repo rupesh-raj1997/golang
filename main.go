7e13cebd6ea91dd412c8bb71c271e9b46c16ba37
zigmint <rupeshr053@gmail.com>
2025-06-10 09:56:57 +0530

    CH11 L1

--- main.go
+++ main.go
@@ -1,44 +1,14 @@
 package main
 
-import "errors"
-
-type customer struct {
-	id      int
-	balance float64
-}
-
-type transactionType string
-
-const (
-	transactionDeposit    transactionType = "deposit"
-	transactionWithdrawal transactionType = "withdrawal"
+import (
+	"fmt"
 )
 
-type transaction struct {
-	customerID      int
-	amount          float64
-	transactionType transactionType
+func test(text string) {
+	fmt.Println(text)
 }
 
-func updateBalance(cust *customer, trans transaction) error {
-	switch trans.transactionType {
-	case transactionDeposit:
-		(*cust).balance += trans.amount
-	case transactionWithdrawal:
-		if trans.amount > (*cust).balance {
-			return errors.New("insufficient funds")
-		}
-		(*cust).balance -= trans.amount
-	default:
-		return errors.New("unknown transaction type")
-	}
-	return nil
+func main() {
+	test("starting Mailio server")
+	test("stopping Mailio server")
 }
-
-// Don't touch above this line
-
-// ?
-// alice := customer{id: 1, balance: 100.0}
-// deposit := transaction{customerID: 1, amount: 50, transactionType: transactionDeposit}
-
-// updateBalance(&alice, deposit)
