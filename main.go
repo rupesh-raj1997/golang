7274b2c3e33137e63daa53ec085403bd060963df
zigmint <rupeshr053@gmail.com>
2025-06-10 20:28:45 +0530

    CH12 L5

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-func addEmailsToQueue(emails []string) chan string {
-	ch := make(chan string, len(emails))
-	for _, email := range emails {
-		ch <- email
+func countReports(numSentCh chan int) int {
+	total := 0
+	var closed = false
+	for !closed {
+		num, ok := <-numSentCh
+		if !ok {
+			closed = true
+		} else {
+			total += num
+		}
 	}
-	return ch
+
+	return total
+}
+
+// don't touch below this line
+
+func sendReports(numBatches int, ch chan int) {
+	for i := 0; i < numBatches; i++ {
+		numReports := i*23 + 32%17
+		ch <- numReports
+	}
+	close(ch)
 }
