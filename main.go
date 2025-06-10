afb3983b86f9365826ed8bee10ca105c6df41f0f
zigmint <rupeshr053@gmail.com>
2025-06-10 12:06:06 +0530

    CH12 L1

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-func test(text string) {
-	fmt.Println(text)
+func sendEmail(message string) {
+	go func() {
+		time.Sleep(time.Millisecond * 250)
+		fmt.Printf("Email received: '%s'\n", message)
+	}()
+	fmt.Printf("Email sent: '%s'\n", message)
+}
+
+// Don't touch below this line
+
+func test(message string) {
+	sendEmail(message)
+	time.Sleep(time.Millisecond * 500)
+	fmt.Println("========================")
 }
 
 func main() {
-	test("starting Mailio server")
-	test("stopping Mailio server")
+	test("Hello there Kaladin!")
+	test("Hi there Shallan!")
+	test("Hey there Dalinar!")
 }
